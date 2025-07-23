050f07b9de3328fee0fda98d679cb7bf102187de
BeriBeli <JeffreyTRYen@gmail.com>
2025-07-23 14:06:21 +0800

    build: add bundle scripts

---
+++ bundle.ps1
@@ -0,0 +1,32 @@
+if (-not (Get-Command pyinstaller -ErrorAction SilentlyContinue)) {
+    Write-Host "pyinstaller is not found. Please source the virtual environment."
+    exit 1
+}
+
+if (-not (Test-Path -Path "jre" -PathType Container)) {
+    Write-Host "Running PyInstaller without JRE..."
+    pyinstaller -F irgen/src/irgen/main.py `
+        --name irgen `
+        --paths irgen/src `
+        --copy-metadata irgen `
+        --add-data "schema/target/ipxact-schema-1.0.0.jar;jar/" `
+        --add-data "schema/target/dependency/*;jar/dependency" `
+        --exclude-module pytest `
+        --exclude-module ruff `
+        --exclude-module mypy `
+        --console `
+}
+else {
+    Write-Host "Running PyInstaller with JRE..."
+    pyinstaller -F irgen/src/irgen/main.py `
+        --name irgen `
+        --paths irgen/src `
+        --copy-metadata irgen `
+        --add-data "jre/;jre/" `
+        --add-data "schema/target/ipxact-schema-1.0.0.jar;jar/" `
+        --add-data "schema/target/dependency/*;jar/dependency" `
+        --exclude-module pytest `
+        --exclude-module ruff `
+        --exclude-module mypy `
+        --console `
+}
---
+++ bundle.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if ! command -v pyinstaller &> /dev/null; then
+    echo "pyinstaller is not found. Please source the virtual environment."
+    exit 1
+fi
+
+if [ ! -d "jre"]; then
+    echo "Running PyInstaller without JRE..."
+    pyinstaller -F irgen/src/irgen/main.py \
+        --name irgen \
+        --paths irgen/src \
+        --copy-metadata irgen \
+        --add-data "schema/target/ipxact-schema-1.0.0.jar:jar/" \
+        --add-data "schema/target/dependency/*:jar/dependency" \
+        --exclude-module pytest \
+        --exclude-module ruff \
+        --exclude-module mypy \
+        --console
+else
+    echo "Running PyInstaller with JRE..."
+    pyinstaller -F irgen/src/irgen/main.py \
+        --name irgen \
+        --paths irgen/src \
+        --copy-metadata irgen \
+        --add-data "jre/:jre/" \
+        --add-data "schema/target/ipxact-schema-1.0.0.jar:jar/" \
+        --add-data "schema/target/dependency/*:jar/dependency" \
+        --exclude-module pytest \
+        --exclude-module ruff \
+        --exclude-module mypy \
+        --console
+fi
+
